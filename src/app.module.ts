98e923aeca7a0ea12291664b169abec0095e3fea
gdpnei2002 <joseneilson25@gmail.com>
2023-01-16 16:52:47 -0300

    crud funcionando

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [MongooseModule.forRoot(
+    'mongodb+srv://JoseNeilson:josenei12@cluster0.7fjojvc.mongodb.net/test'
+    ),UsersModule
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
